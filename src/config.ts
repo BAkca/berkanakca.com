0c13ad35f4e4cf40a17d1990c2bf2c75cba2b792
BAkca <berkanakca@gmail.com>
2025-05-20 03:27:19 +0300

    Fix: removed invalid TRANSITION_API import

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const SITE_TITLE = "Berkan Akça | Performance Marketing Manager";
 export const SITE_DESCRIPTION = "Growth-driven marketing leader with expertise in Google Ads, Meta, Criteo, and Amazon Ads. Specialized in building high-ROI digital strategies across industries.";
 export const OG_IMAGE = "/og-cover.webp"; // We’ll add this image next
-export const TRANSITION_API = true; // or false if you're not using Astro View Transitions
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
